cd783b48d6f2e942a5fc696e311471a645760f60
ahmetkca <ahmet.karapinar@ontariotechu.net>
2022-08-03 21:55:09 -0400

    Setting up mock data to demo Container Component Design Pattern

---
+++ src/ProductInfo.jsx
@@ -0,0 +1,14 @@
+export const ProductInfo = ({ product }) => {
+    
+    const { name, price, description, rating } = product;
+    
+    return (
+        <>
+            <h3>{name}</h3>
+            <p>Price: {price}</p>
+            <h4>Description:</h4>
+            <p>{description}</p>
+            <p>Average Rating: {rating}</p>
+        </>
+    );
+}
---
+++ src/UserInfo.jsx
@@ -0,0 +1,16 @@
+export const UserInfo = ({ user }) => {
+    
+        const { name, age, hairColor, hobbies } = user;
+    
+        return (
+            <>
+                <h3>{name}</h3>
+                <p>Age: {age}</p>
+                <p>Hair Color: {hairColor}</p>
+                <h4>Hobbies:</h4>
+                <ul>
+                    {hobbies.map((hobby, key) => <li key={key + hobby}>{hobby}</li>)}
+                </ul>
+            </>
+        );
+    }
